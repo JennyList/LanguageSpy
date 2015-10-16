7ef18e1a4560fd60c211efea70649e3e42762538
Jenny <jenny@Dell640m.home>
2015-10-16 22:07:52 +0100

    freq_pi version updated to 0.72

--- RaspberryPi/rf/freq_pi/freq_pi.c
+++ RaspberryPi/rf/freq_pi/freq_pi.c
@@ -4,7 +4,6 @@
 					
 	This code contains parts of code from Pifm.c 
 
-
 Start GPL license:
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
